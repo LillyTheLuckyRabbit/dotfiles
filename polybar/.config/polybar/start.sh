425b6ca3d64ec37d9eca9958c78e446117896e79
Will Coar <williamccoar@gmail.com>
2020-07-07 11:59:24 -0400

    update config

--- polybar/.config/polybar/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-/home/will/.config/wpg/wp_init.sh
-killall Thunar &
-polybar main &
-polybar tray &
-birdtray
--- wpg/.config/wpg/wp_init.sh
+++ wpg/.config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'revolverocelot.png' 'revolverocelot.png'
+wpg -rs 'Persona4.png' 'Persona4.png'
